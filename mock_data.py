ef2069ffd5ac5786d5dc48a326bee950b6828d01
kternity <kternity@gmail.com>
2023-04-27 14:06:30 -0700

    Posted products to MongoDB

--- mock_data.py
+++ mock_data.py
@@ -3,83 +3,72 @@ catalog = [
       "title": "Board 1",
       "category": "Short",
       "price": 2.99,
-      "image": "board_01.webp",
-      "_id": "asdlkhg1123j1"
+      "image": "board_01.webp"
    },
    {
       "title": "Board 2",
       "category": "Short",
       "price": 1.99,
-      "image": "board_02.webp",
-      "_id": "asgdlkh1123j2"
+      "image": "board_02.webp"
    },
    {
       "title": "Board 3",
       "category": "Short",
       "price": 12.99,
-      "image": "board_03.webp",
-      "_id": "asdlkh11g23j3"
+      "image": "board_03.webp"
    },
    {
       "title": "Board 4",
       "category": "Medium",
       "price": 12.99,
-      "image": "board_04.webp",
-      "_id": "asdlkh1123gj4"
-   },  {
+      "image": "board_04.webp"
+   },  
+   {
       "title": "board 5",
       "category": "Medium",
       "price": 12.99,
-      "image": "board_05.webp",
-      "_id": "asdlkh1123fgj5"
+      "image": "board_05.webp"
    },
    {
       "title": "Board 6",
       "category": "Medium",
       "price": 12.99,
-      "image": "board_06.webp",
-      "_id": "asdlkh1123jg6"
+      "image": "board_06.webp"
    },
    {
       "title": "Board 7",
       "category": "Long",
       "price": 12.99,
-      "image": "board_07.webp",
-      "_id": "asdlkh1123j7"
+      "image": "board_07.webp"
    },
    {
       "title": "Board 8",
       "category": "Short",
       "price": 12.99,
-      "image": "board_08.webp",
-      "_id": "asdlkh11233j8"
+      "image": "board_08.webp"
    },
    {
       "title": "Board 9",
       "category": "Medium",
       "price": 12.99,
-      "image": "board_09.webp",
-      "_id": "asdlkh1323j9"
+      "image": "board_09.webp"
    },
    {
       "title": "Board 10",
       "category": "Long",
       "price": 12.99,
-      "image": "board_10.webp",
-      "_id": "asdlkh51123j10"
+      "image": "board_10.webp"
    },
    {
       "title": "Board 9",
       "category": "Long",
       "price": 12.99,
-      "image": "board_11.webp",
-      "_id": "asdlksh1123j"
+      "image": "board_11.webp"
    },
    {
       "title": "Board 10",
       "category": "Medium",
       "price": 12.99,
-      "image": "board_12.webp",
-      "_id": "asdlnkh1123j"
+      "image": "board_12.webp"
    }
 ]
--- server.py
+++ server.py
@@ -9,13 +9,15 @@ app = Flask("server")
 def home():
     return "hello from flask"
 
-@app.get("/test")
-def test():
-    return "this is another endpoint"
+
 
 @app.get("/about")
 def about():
     return "About Kenneth Chung"
+
+
+
+# CATALOG API 
 
 @app.get("/api/version")
 def version():
@@ -27,11 +29,15 @@ def version():
    return json.dumps(version)
 
 
-
-
 @app.get("/api/catalog")
 def get_catalog():
-    return json.dumps(catalog)
+    cursor = db.products.find({})
+    results = []
+    for prod in cursor:
+        prod["_id"] = str(prod["_id"]) 
+        results.append(prod)
+
+    return json.dumps(results)
 
 
 #save products
@@ -47,51 +53,56 @@ def save_product():
 
 @app.get("/api/catalog/<category>")
 def get_by_category(category):
-   result = []
-   for prod in catalog:
-       if prod["category"].lower() == category.lower():
-           result.append(prod)
-   
-   return json.dumps(result)
+    cursor = db.products.find({"category": category})
+    results = []
+    for prod in cursor:
+        prod["_id"] = str(prod["_id"])
+        results.append(prod)
+
+    return json.dumps(results)
+
 
 @app.get("/api/catalog/search/<title>")
 def search_by_title(title):
-   result = []
-   for prod in catalog:
-       if title.lower() in prod["title"].lower():
-           result.append(prod)
-   return json.dumps(result)
+    cursor = db.products.find({"title": {"$regex": title, "$options": "i"} })
+    results = []
+    for prod in cursor:
+        prod["_id"] = str(prod["_id"])
+        results.append(prod)
+
+    return json.dumps(results)
+
+
 
 @app.get("/api/product/cheaper/<price>")
 def search_by_price(price):
-   result = []
-   for prod in catalog:
-       if prod["price"] < float(price):
-           result.append(prod)
-   return json.dumps(result)
+    cursor = db.products.find({})
+    result = []
+    for prod in cursor:
+        if prod["price"] < float(price):
+            prod["_id"] = str(prod["_id"])
+            result.append(prod)
+    
+    return json.dumps(result)
 
 
 @app.get("/api/product/count")
 def count_products():
-   count = len(catalog)
-   return json.dumps(count)
-
-
-
-
-
-
-
+    count = db.products.count_documents({})
+    return json.dumps(count)
 
 
 
 @app.get("/api/product/cheapest")
 def get_cheapest():
-   answer = catalog[0]
-   for prod in catalog:
-       if prod["price"] < answer["price"]:
-           answer = prod
-   return json.dumps(answer)
+    cursor = db.products.find({})
+    answer = cursor[0]
+    for prod in cursor:
+        if prod["price"] < answer["price"]:
+            answer = prod
+    
+    answer["_id"] = str(answer["_id"])
+    return json.dumps(answer)
 
 
 @app.get('/test/numbers')
